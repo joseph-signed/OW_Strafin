57468ba7e3277b909442a0665024aff1d60b0296
joseph-signed <105678044+joseph-signed@users.noreply.github.com>
2022-09-07 19:25:04 -0500

    implement gamepad js functionality into original desktop.ts class

--- ts/src/desktop/desktop.ts
+++ ts/src/desktop/desktop.ts
@@ -1,8 +1,38 @@
 import { AppWindow } from "../AppWindow";
 import { kWindowNames } from "../consts";
 
-// The desktop window is the window displayed while game is not running.
-// In our case, our desktop window has no logic - it only displays static data.
-// Therefore, only the generic AppWindow class is called.
-new AppWindow(kWindowNames.desktop);
+class Desktop extends AppWindow {
+    private static _instance: Desktop;
 
+    private constructor() {
+        super(kWindowNames.desktop);
+    }
+
+    public static instance() {
+        if (!this._instance) {
+            this._instance = new Desktop();
+        }
+
+        return this._instance;
+    }
+
+    public async run() {
+        //   const gameClassId = await this.getCurrentGameClassId();
+
+        //   const gameFeatures = kGamesFeatures.get(gameClassId);
+
+        //   if (gameFeatures && gameFeatures.length) {
+        //     this._gameEventsListener = new OWGamesEvents(
+        //       {
+        //         onInfoUpdates: this.onInfoUpdates.bind(this),
+        //         onNewEvents: this.onNewEvents.bind(this)
+        //       },
+        //       gameFeatures
+        //     );
+
+        //     this._gameEventsListener.start();
+        //   }
+    }
+}
+
+Desktop.instance().run();
